45c37eb27ad5349f498b312a15f8141f7ddb2d0b
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-02-19 11:47:43 -0300

    feat: increase jwt expiration to 365d

--- src/config/jwt.config.js
+++ src/config/jwt.config.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 module.exports = {
     secret: process.env.JWT_SECRET || "default_secret",
-    jwtExpiration: process.env.JWT_EXPIRATION || "24h", // 24 hours
-    jwtRefreshExpiration: 86400, // 24 hours
+    jwtExpiration: process.env.JWT_EXPIRATION || "365d", // 1 year
+    jwtRefreshExpiration: 31536000, // 1 year in seconds
 };
